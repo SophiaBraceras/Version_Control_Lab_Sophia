dde79e8af37de40f80709f9dee8f4312dda0b045
etha-ngom <ethangomez@ufl.edu>
2023-10-25 10:15:12 -0400

    Added decode() function - Ethan

--- version_control_p2.py
+++ version_control_p2.py
@@ -19,8 +19,22 @@ def encoder(code):
 # number = input("Number to encode:")
 # print(encoder(number))
 
-def decoder():
-    pass
+
+# This function was written by Ethan Gomez
+def decode(encoded_pass):
+    print(f"Entered Pass: {encoded_pass}")
+    encoded_pass = list(encoded_pass)
+
+    # Loops through every index in the encoded password
+    for index in range(len(encoded_pass)):
+        encoded_pass[index] = int(encoded_pass[index]) - 3
+        # If the -3 made it negative, add 10.
+        if encoded_pass[index] < 0:
+            encoded_pass[index] += 10
+        encoded_pass[index] = str(encoded_pass[index])
+
+    return "".join(encoded_pass)
+
 def menu():
     print("Menu")
     print("-------------")
@@ -35,12 +49,13 @@ def main():
         option = input("Please enter an option: ")
         if option == "1":
             num = input("Please enter your password to encode: ")
-            encoder(num)
+            # encoded variable created by Ethan for the decoder to work
+            encoded = encoder(num)
             print("Your password has been encoded and stored!")
 
         if option == "2":
             # num = input("Please enter your password to encode: ")
-            print(f"The encoded password is {encoder(num)} and the original password is {decoder(num)}")
+            print(f"The encoded password is {encoder(num)} and the original password is {decoder(encoded)}") # decoder(num) changed by Ethan to decoder(encoded)
 
 
         if option == "3":
